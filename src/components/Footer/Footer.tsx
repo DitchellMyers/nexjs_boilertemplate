2e7bc4db899bbfa02ad254a9e2511120e989d7ab
DitchellMyers <may-pascalrene@web.de>
2022-07-26 00:51:47 +0200

    feat(footer): Add Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import Container from "../../ui/Container"
+
+const Footer = () => {
+    return <Container></Container>
+}
+
+export default Footer
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import apolloClient from '../lib/apollo'
+import Head from '../components/Head/Head'
+import Footer from '../components/Footer/Footer'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ApolloProvider client={apolloClient}>
+      <Head></Head>
       <Component {...pageProps} />
+      <Footer></Footer>
     </ApolloProvider>
   )
 }
